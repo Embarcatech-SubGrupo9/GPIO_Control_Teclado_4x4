e76ae45165211af6d17056b48ab166f44ec8455c
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2025-01-18 23:58:30 -0300

    organizando o código das branches
    Foi organizando as branches lads com a maim.

--- GPIO_Control_Teclado_4x4.c
+++ GPIO_Control_Teclado_4x4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Constantes
 const int colunas = 4;
 const int linhas = 4;
 const int leds = 3;
@@ -16,52 +17,94 @@ const char mapa_tecla[4][4] = {
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
-void inicializar_pinos() {
+// Protótipos das funções
+void inicializar_pinos();
+void controlar_leds(char tecla);
+char verificar_tecla();
 
-    for (int i = 0; i < leds; i++) {
+int main()
+{
+    stdio_init_all();
+    inicializar_pinos();
+
+    while (true)
+    {
+        char tecla = verificar_tecla();
+        if (tecla != '\0')
+        {
+            controlar_leds(tecla);
+            printf("Tecla pressionada: %c\n", tecla);
+            sleep_ms(100); // Debounce
+        }
+    }
+}
+
+// Função para verificar qual tecla foi pressionada
+char verificar_tecla()
+{
+    for (int col = 0; col < colunas; col++)
+    {
+        gpio_put(coluna_pins[col], 1);
+        for (int lin = 0; lin < linhas; lin++)
+        {
+            if (gpio_get(linha_pins[lin]) == 0)
+            {
+                gpio_put(coluna_pins[col], 0);
+                return mapa_tecla[lin][col];
+            }
+        }
+        gpio_put(coluna_pins[col], 0);
+    }
+    return '\0'; // Retorna '\0' se nenhuma tecla for pressionada
+}
+
+// Função para inicializar os pinos
+void inicializar_pinos()
+{
+    for (int i = 0; i < leds; i++)
+    {
         gpio_init(led_pin[i]);
         gpio_set_dir(led_pin[i], GPIO_OUT);
         gpio_put(led_pin[i], 0);
     }
-    for (int i = 0; i < colunas; i++) {
+    for (int i = 0; i < colunas; i++)
+    {
         gpio_init(coluna_pins[i]);
         gpio_set_dir(coluna_pins[i], GPIO_OUT);
         gpio_put(coluna_pins[i], 1);
     }
-    for (int i = 0; i < linhas; i++) {
+    for (int i = 0; i < linhas; i++)
+    {
         gpio_init(linha_pins[i]);
         gpio_set_dir(linha_pins[i], GPIO_IN);
         gpio_pull_up(linha_pins[i]);
     }
 }
 
-void controlar_leds(char tecla) {
-    for (int i = 0; i < leds; i++) {
-        gpio_put(led_pin[i],0);
-    }
-    if (tecla == 'A') {
-        gpio_put(led_pin[0],1);
-    } else if (tecla == 'B') {
-        gpio_put(led_pin[1],1);  
-    } else if (tecla == 'C') {
-        gpio_put(led_pin[2],1 );
-    } else if (tecla == 'D') { // função para ligar todos os leds
-        for (int i = 0; i< leds; i++) {
-            gpio_put(led_pin[i],1);
-        }
-    }
-}
-
-int main()
+// Função para controlar os LEDs
+void controlar_leds(char tecla)
 {
-    stdio_init_all();
-    while (true)
+    for (int i = 0; i < leds; i++)
     {
-        if (tecla) {
-            controlar_leds(tecla);
-            printf("Tecla pressionada: %c\n", tecla);
-            controlar_leds(tecla);
-            sleep_ms(100);
+        gpio_put(led_pin[i], 0);
+    }
+    if (tecla == 'A')
+    {
+        gpio_put(led_pin[0], 1);
+    }
+    else if (tecla == 'B')
+    {
+        gpio_put(led_pin[1], 1);
+    }
+    else if (tecla == 'C')
+    {
+        gpio_put(led_pin[2], 1);
+    }
+    else if (tecla == 'D')
+    { // Ligar todos os LEDs
+        for (int i = 0; i < leds; i++)
+        {
+            gpio_put(led_pin[i], 1);
         }
     }
 }
